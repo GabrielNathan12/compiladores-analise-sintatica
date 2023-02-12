dac9fef94b84de142ddf0fe3d5f6e25b8d489aa9
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2023-02-11 21:25:13 -0300

    Leitura de var e terminais no arquivo feita

--- criacao_de_Tabelas/SLR1.py
+++ criacao_de_Tabelas/SLR1.py
@@ -1,13 +1,64 @@
-regras = []
-with open("gramatica.txt") as arq:
+import re
+from typing import List, Set
+
+def getTerminal(var):
+    x = re.compile('[a-z+*/-]');
+    q = x.findall(var);
     
+    if q:
+        return q;
+        
+
+def getNaoTerminal(var):
+    x = re.compile('[A-Z]');
+    q = x.findall(var);
+    
+    if q:
+        return q;
+        
+        
+regras = []
+#getTerminal("G+a*briel");
+
+with open("criacao_de_tabelas/gramatica.txt") as arq:
     for linha in arq:
         linha  = linha.strip();
         regras.append(linha)
 
-print("Digite os estados não terminais da gramática");
 
-print(regras);
-#print(regras)
-input();
+auxNaoTerminais = []
+
+with open ("criacao_de_tabelas/gramatica.txt") as arq:   
+    for linha in arq:
+        linha  = linha.split("->");
+        for i in linha:
+            if getNaoTerminal(i) != None:
+                auxNaoTerminais.append(getNaoTerminal(i));
+        
+NaoTerminal = []
+for i in auxNaoTerminais:
+    for j in i:
+        NaoTerminal.append(j);
+
+
+auxTerminais = []
+
+with open ("criacao_de_tabelas/gramatica.txt") as arq:   
+    for linha in arq:
+        linha  = linha.split("->");
+        for i in linha:
+            if getTerminal(i) != None:
+                auxTerminais.append(getTerminal(i));
+        
+Terminal = []
+for i in auxTerminais:
+    for j in i:
+        Terminal.append(j);
+
+NaoTerminal = list(set(NaoTerminal));
+Terminal = list(set(Terminal));
+
+print("Terminais: ",Terminal);
+print("Nao Terminais: ", NaoTerminal);
+
 arq.close();
